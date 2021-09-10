efba1d2924ac4f0f0aba59b2f2fca3650600f25b
Everton Dutra <evertondutra02@gmail.com>
2021-09-10 02:38:56 -0300

    Completei o Script excluirPet (Delete)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -93,4 +93,23 @@ public class Pet {
         ;
     }
 
+    @Test(priority = 4)
+    public void excluirPet(){
+        String petId = "20022020";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .delete(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is((petId)))
+        ;
+
+    }
+
 }
